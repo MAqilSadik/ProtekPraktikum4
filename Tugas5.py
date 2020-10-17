5b7566226a470c7ea87f8c3842abfd533822c931
MAqilSadik <aqilsadik@users.noreply.github.com>
2020-10-17 10:52:07 +0700

    Create Tugas5.py

---
+++ Tugas5.py
@@ -0,0 +1,7 @@
+print("Grafik diagram batang horizontal mahasiswa laki laki dan perempuan")
+LakiLaki = int(input("jumlah mahasiswa laki laki : "))
+Perempuan = int(input("jumlah mahasiswa perempuan : "))
+
+
+print("laki laki : ", '*' * LakiLaki)
+print("perempuan : ", '*' * Perempuan)
